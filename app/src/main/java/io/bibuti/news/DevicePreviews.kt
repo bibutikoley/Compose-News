f124aea4ae87acda9905bdd183d7b03b9cb186ad
Bibuti Koley <bibutikoley@outlook.com>
2022-07-24 11:45:10 +0530

    Initial Commit

---
+++ app/src/main/java/io/bibuti/news/DevicePreviews.kt
@@ -0,0 +1,8 @@
+package io.bibuti.news
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true, showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+annotation class DevicePreviews() {}
